5135da0f209467f88931385f92778bf70ffb24b1
nmathias2101@gmail.com <nmathias2101@gmail.com>
2023-10-05 00:06:57 -0700

    change filename in url

--- pytest/test_selenium.py
+++ pytest/test_selenium.py
@@ -8,30 +8,38 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import staleness_of
 from selenium.webdriver.common.by import By
 
+def construct_headless_chrome_driver():
+    options = Options()
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
+    return webdriver.Chrome(options=options)
+
+
+def get_landing_page_url():
+    test_dir = os.getcwd()
+    index_path = os.path.join(test_dir, "web-page", "parent_page.html")
+    index_uri = Path(index_path).as_uri()
+    return index_uri
+
+
+# As demonstrated in the linked web page from the course assignment
 @contextmanager
 def wait_for_page_load(driver, timeout=30):
     old_page = driver.find_element_by_tag_name('html')
     yield
     WebDriverWait(driver, timeout).until( staleness_of(old_page) )
 
+
 def test_nonsecret_scenario():
-    # get landing page url
-    test_dir = os.getcwd()
-    index_path = os.path.join(test_dir, "page", "index.html")
-    landing_page = Path(index_path).as_uri()
+    landing_page =  get_landing_page_url()
+    driver = construct_headless_chrome_driver()
 
-    # construct headless chrome driver
-    options = Options()
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
     driver.get(landing_page)
-
     wait_for_page_load(driver)
 
-    # fill the form and submit
+    # You can place additional test code here to drive this one test
     preferred_name = driver.find_element(By.ID, "preferredname")
     preferred_name.send_keys('Neil')
 
@@ -48,7 +56,7 @@ def test_nonsecret_scenario():
 
     k = driver.current_url
     assert 'web-page/response_page.html?preferredname=Neil&food=pizza&secret=imagine' in k
-    
+
     thank_name = driver.find_element(By.ID, 'thankname')
     assert thank_name.text == 'Neil'
 
