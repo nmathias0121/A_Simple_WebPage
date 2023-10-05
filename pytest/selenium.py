a0cf4730b867d5ca85af1aa63de54c15d70370b4
nmathias2101@gmail.com <nmathias2101@gmail.com>
2023-10-04 23:53:06 -0700

    fill form, submit and assert

--- pytest/selenium.py
+++ pytest/selenium.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from contextlib import contextmanager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import staleness_of
+from selenium.webdriver.common.by import By
 
 @contextmanager
 def wait_for_page_load(driver, timeout=30):
@@ -14,7 +15,6 @@ def wait_for_page_load(driver, timeout=30):
     WebDriverWait(driver, timeout).until( staleness_of(old_page) )
 
 def test_nonsecret_scenario():
-
     # get landing page url
     test_dir = os.getcwd()
     index_path = os.path.join(test_dir, "page", "index.html")
@@ -30,3 +30,29 @@ def test_nonsecret_scenario():
     driver.get(landing_page)
 
     wait_for_page_load(driver)
+
+    # fill the form and submit
+    preferred_name = driver.find_element(By.ID, "preferredname")
+    preferred_name.send_keys('Neil')
+
+    food = driver.find_element(By.ID,'food')
+    food.send_keys('pizza')
+
+    secret = driver.find_element(By.ID,'secret')
+    secret.send_keys('imagine')
+
+    submit_button = driver.find_element(By.ID,'submit')
+    submit_button.click()
+
+    wait_for_page_load(driver)
+
+    k = driver.current_url
+    assert 'web-page/response_page.html?preferredname=Neil&food=pizza&secret=imagine' in k
+    
+    thank_name = driver.find_element(By.ID, 'thankname')
+    assert thank_name.text == 'Neil'
+
+    food_ploy = driver.find_element(By.ID, 'foodploy')
+    assert food_ploy.text == 'pizza'
+
+    driver.quit()
